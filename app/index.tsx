d2930331529278184f4aabbc4d5052fc1c887129
ronyhe <ronyheruth@wix.com>
2024-09-29 20:22:53 +0300

    Install MUI

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Main } from '#comps/Main'
 import { createRoot } from 'react-dom/client'
+import { CssBaseline } from '@mui/material'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 document.addEventListener('DOMContentLoaded', main)
 
 async function main() {
     const root = createRoot(document.querySelector('#app')!)
-    root.render(<Main />)
+    root.render(
+        <CssBaseline>
+            <Main />
+        </CssBaseline>
+    )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { add } from '#logic/math'
+import { Box, Button, Typography } from '@mui/material'
 
 export function Main() {
     return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>2 + 2 = {add(2, 2)}</h2>
-        </div>
+        <Box>
+            <Typography variant='h1'>Hello, world!</Typography>
+            <Typography variant='h2'>2 + 2 = {add(2, 2)}</Typography>
+            <Button variant='contained' color='primary'>
+                Click me!
+            </Button>
+        </Box>
     )
 }
